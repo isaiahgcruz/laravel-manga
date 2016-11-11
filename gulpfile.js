a42f493076cbcd88a4ea48f4b00734d37c2a760a
Isaiah Cruz <isaiahgcruz@gmail.com>
2016-11-11 18:31:23 +0800

    Added browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -15,5 +15,9 @@ require('laravel-elixir-vue-2');
 
 elixir(mix => {
     mix.sass('app.scss')
-       .webpack('app.js');
+       .webpack('app.js')
+       .browserSync({
+            notify: false,
+            proxy: 'manga.dev'
+        });
 });
