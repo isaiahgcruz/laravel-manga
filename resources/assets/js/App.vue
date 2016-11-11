9b8d722be7e2d59a8de09f4b398764d852351ef6
Isaiah Cruz <isaiahgcruz@gmail.com>
2016-11-11 18:40:47 +0800

    Initialized app.js and added placeholder App.vue

---
+++ resources/assets/js/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="container">
+    App
+  </div>
+</template>
+
+<style lang="css">
+  .container {
+    padding-right: 15px;
+    padding-left: 15px;
+    margin-right: auto;
+    margin-left: auto;
+  }
+</style>
